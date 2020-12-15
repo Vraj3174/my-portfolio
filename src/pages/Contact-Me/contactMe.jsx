04e473fb578e4fb4c87fdba4c8cb6b85fc025582
Vraj Patel <v227patel@uwaterloo.ca>
2020-12-15 12:30:13 -0500

    Contact me Design v1

--- src/pages/Contact-Me/contactMe.jsx
+++ src/pages/Contact-Me/contactMe.jsx
@@ -7,29 +7,60 @@ export default function Contact() {
     return(
         <>
             <Header/>
-            <form 
-                method="post"
-                name="contact"   
-                data-netlify="true"
-                onSubmit="submit"
-            >
-                <input type="hidden" name="form-name" value="contact"/>
-                <p>
-                    <label>
-                        Name 
-                        <input type="text" name="name" id="name" placeholder="Your Name"/>
+
+            <div className="container">
+                <h1>Contact Form</h1>
+
+                <form 
+                    method="post"
+                    name="contact"   
+                    data-netlify="true"
+                    onSubmit="submit"
+                >
+                    <input type="hidden" name="form-name" value="contact"/>
+
+                    <label for="firstName">
+                        First Name
+                        <input type="text" id="firstName" name="firstName" placeholder="Your First Name"/>
                     </label>
-                </p>
-                <p>
-                    <label>Email <input type="email" name="email" id="email" placeholder="Your E-mail"/></label>
-                </p>
-                <p>
-                    <label>Message <input type="info" name="info" id="info" placeholder="Your Message"/></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
-            </form>
+                    <label for="lastName">
+                        Last Name
+                        <input type="text" id="lastName" name="lastName" placeholder="Your Last Name"/>
+                    </label>
+                    <label for="Message">
+                        Message
+                        <input type="text" id="message" name="message" placeholder="Enter a Message"/>
+                    </label>
+
+                    <button className="button type2" type="submit">Send</button>
+                </form>
+            </div>
+            
+            {/* <div className="container">
+                <form 
+                    method="post"
+                    name="contact"   
+                    data-netlify="true"
+                    onSubmit="submit"
+                >
+                    <input type="hidden" name="form-name" value="contact"/>
+                    <p>
+                        <label>
+                            Name 
+                            <input type="text" name="name" id="name" placeholder="Your Name"/>
+                        </label>
+                    </p>
+                    <p>
+                        <label>Email <input type="email" name="email" id="email" placeholder="Your E-mail"/></label>
+                    </p>
+                    <p>
+                        <label>Message <input type="info" name="info" id="info" placeholder="Your Message"/></label>
+                    </p>
+                    <p>
+                        <button className="button type2" type="submit">Send</button>
+                    </p>
+                </form>
+            </div> */}
             <Footer/>
         </>
     )

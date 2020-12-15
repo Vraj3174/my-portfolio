3df02bd887f4571e7fd21f2608a9fca9458bcef6
Vraj Patel <v227patel@uwaterloo.ca>
2020-12-15 13:46:32 -0500

    Update contactMe.jsx

--- src/pages/Contact-Me/contactMe.jsx
+++ src/pages/Contact-Me/contactMe.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./contactMeStyles.scss";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
-import {IoMdContact} from 'react-icons/Io';
+import {FiMail} from 'react-icons/fi';
 
 export default function Contact() {
     return(
@@ -12,7 +12,7 @@ export default function Contact() {
             <div className="container">
                 <div className="container-contact">
                     <div className="container">
-                        <IoMdContact size="15%" className="icon"/>
+                        <FiMail size="15%" className="icon"/>
                         <h1 className="name">Contact Me</h1>
                     </div>
 

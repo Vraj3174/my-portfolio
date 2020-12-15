d557a5ab68558d5af3aff13dfcba912d955b5ed7
Vraj Patel <v227patel@uwaterloo.ca>
2020-12-15 13:09:32 -0500

    contact me design v2

--- src/pages/Contact-Me/contactMe.jsx
+++ src/pages/Contact-Me/contactMe.jsx
@@ -7,64 +7,40 @@ export default function Contact() {
     return(
         <>
             <Header/>
-
+            <br/>
             <div className="container">
-                <h1>Contact Form</h1>
+                <div className="container-contact">
+                    <div>
+                        <h1 className="name">Contact Form</h1>
+                    </div>
 
-                <form 
-                    method="post"
-                    name="contact"   
-                    data-netlify="true"
-                    onSubmit="submit"
-                >
-                    <input type="hidden" name="form-name" value="contact"/>
+                    <form 
+                        method="post"
+                        name="contact"   
+                        data-netlify="true"
+                        onSubmit="submit"
+                    >
+                        <input type="hidden" name="form-name" value="contact"/>
 
-                    <label for="firstName">
-                        First Name
-                        <input type="text" id="firstName" name="firstName" placeholder="Your First Name"/>
-                    </label>
-                    <label for="lastName">
-                        Last Name
-                        <input type="text" id="lastName" name="lastName" placeholder="Your Last Name"/>
-                    </label>
-                    <label for="email">
-                        E-mail
-                        <input type="text" id="email" name="email" placeholder="Your E-mail"/>
-                    </label>
-                    <label for="Message">
-                        Message
-                        <input type="text" id="message" name="message" placeholder="Enter a Message"/>
-                    </label>
-
-                    <button className="button type2" type="submit">Send</button>
-                </form>
-            </div>
-            
-            {/* <div className="container">
-                <form 
-                    method="post"
-                    name="contact"   
-                    data-netlify="true"
-                    onSubmit="submit"
-                >
-                    <input type="hidden" name="form-name" value="contact"/>
-                    <p>
-                        <label>
-                            Name 
-                            <input type="text" name="name" id="name" placeholder="Your Name"/>
+                        <label className="name" for="name">
+                            Name
+                            <input type="text" id="name" name="name" placeholder="Your Name"/>
                         </label>
-                    </p>
-                    <p>
-                        <label>Email <input type="email" name="email" id="email" placeholder="Your E-mail"/></label>
-                    </p>
-                    <p>
-                        <label>Message <input type="info" name="info" id="info" placeholder="Your Message"/></label>
-                    </p>
-                    <p>
-                        <button className="button type2" type="submit">Send</button>
-                    </p>
-                </form>
-            </div> */}
+                        <label className="name" for="email">
+                            E-mail
+                            <input type="text" id="email" name="email" placeholder="Your E-mail"/>
+                        </label>
+                        <label className="name" for="Message">
+                            Message
+                            <input type="text" id="message" name="message" placeholder="Enter a Message"/>
+                        </label>
+
+                        <div className="container">
+                            <button className="button type2 submit" type="submit">Send</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
             <Footer/>
         </>
     )

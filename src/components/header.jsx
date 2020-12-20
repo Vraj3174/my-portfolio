15cde35c419170b218715077037983e1e1266180
Vraj Patel <v227patel@uwaterloo.ca>
2020-12-20 13:21:24 -0500

    Responsive navabr

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,58 @@
 import React from 'react';
 import "../styles/headerStyles.scss";
+import ResponsiveMenu from 'react-responsive-navbar';
+
+import { CgMenu, CgClose } from 'react-icons/cg';
 
 export default function Header() {
 
     return (
-        <ul className="ul">
-            <li className="li-left">
-                <form>
-                    <button formAction="/" className="button-header type-header">Home</button>
-                </form> 
-            </li>
-            <li className="li-right">
-                <form>
-                    <button formAction="/About-Me/aboutMe" className="button-header type-header">About Me</button>
-                    <button formAction="/Portfolio/portfolio" className="button-header type-header">Portfolio</button>
-                    <button formAction="/Contact-Me/contactMe" className="button-header type-header">Contact Me</button>
-                </form>
-            </li>
-        </ul>
+
+        <ResponsiveMenu
+            menuOpenButton={<CgMenu size="10%" color="rgb(216, 49, 90)"/>}
+            menuCloseButton={<CgClose size="10%" color="rgb(216, 49, 90)"/>}
+            changeMenuOn="500px"
+            largeMenuClassName="largeMenu"
+            smallMenuClassName="ul"
+            menu={
+                <ul className="ul">
+                    <li>
+                        <form>
+                            <button formAction="/" className="button-header type-header">Home</button>
+                        </form> 
+                    </li>
+                    <li>
+                        <form>
+                            <button formAction="/About-Me/aboutMe" className="button-header type-header">About Me</button>
+                        </form>
+                    </li>
+                    <li>
+                        <form>
+                            <button formAction="/Portfolio/portfolio" className="button-header type-header">Portfolio</button>
+                        </form>
+                    </li>
+                    <li>
+                        <form>
+                            <button formAction="/Contact-Me/contactMe" className="button-header type-header">Contact Me</button>
+                        </form>
+                    </li>
+                </ul>
+            }
+        />
+
+        // <ul className="ul">
+        //     <li className="li-left">
+        //         <form>
+        //             <button formAction="/" className="button-header type-header">Home</button>
+        //         </form> 
+        //     </li>
+        //     <li className="li-right">
+        //         <form>
+        //             <button formAction="/About-Me/aboutMe" className="button-header type-header">About Me</button>
+        //             <button formAction="/Portfolio/portfolio" className="button-header type-header">Portfolio</button>
+        //             <button formAction="/Contact-Me/contactMe" className="button-header type-header">Contact Me</button>
+        //         </form>
+        //     </li>
+        // </ul>
     )
 }
